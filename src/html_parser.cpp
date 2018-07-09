2015e5d28b3e697988bf4ff191992205fd73f61d
Rafael Lourenço de Lima Chehab <chehabrafael@gmail.com>
2018-07-09 19:44:21 -0300

    Debugging html_parser
    Signed-off-by: Rafael Lourenço de Lima Chehab <chehabrafael@gmail.com>

--- src/html_parser.cpp
+++ src/html_parser.cpp
@@ -43,7 +43,13 @@ char *concat_path(char *path_base, char *path_relat) {
 
         deque<string> st;
 
-        char buff[50000];
+        //char buff[50000];
+
+        char *buff = (char *) malloc(5000 * sizeof(char));
+
+        if (buff == NULL)
+                return NULL;
+
         char temp[100];
         buff[0] = '\0';
 
@@ -60,9 +66,11 @@ char *concat_path(char *path_base, char *path_relat) {
                 sscanf(buff, "%*c%s", buff);
 
         char barra;
+        int ret;
 
-        printf("\t Buff: %s\n", buff);
-        while (sscanf(buff, "%[^/]%c%s", temp, &barra, buff) == 1) {
+        printf("\tBuff: %s\n", buff);
+        while ((ret = sscanf(buff, "%[^/]%c", temp, &barra)) >= 1) {
+                //printf("\tTemp: %s\tBarra: %c\tBuff: %s\tRet: %d\n", temp, barra, buff, ret);
 
                 if (strcmp(temp, "..") == 0) {
                         st.pop_back();
@@ -71,9 +79,14 @@ char *concat_path(char *path_base, char *path_relat) {
                         st.push_back(t);
                 }
 
-                if (buff == NULL && barra == '/') {
+                int ha_buffer = sscanf(buff, "%*[^/]%*c%s", buff);
+
+                if (ret == 2 && ha_buffer < 1 && barra == '/') {
+                        printf("\t/////\n");
                         st.back().append("/");
                 }
+                if (ret != 2 || ha_buffer < 1)
+                        break;
         }
         char *resp = (char *) malloc(5000 * sizeof(char));
 
@@ -81,11 +94,12 @@ char *concat_path(char *path_base, char *path_relat) {
         resp[1] = '\0';
 
         while (!st.empty()) {
+                printf("\t Piece of URL: %s\n", st.front().c_str());
                 strcat(resp, st.front().c_str());
+                st.pop_front();
                 if (!st.empty())
                         strcat(resp, "/");
 
-                st.pop_front();
         }
 
         return resp;
@@ -140,14 +154,17 @@ vector<pair<string, string> > get_ref_url(char *data, char *url, char *path) {
 
         string s_url(url), s_path(path);
 
-        stack<string> tags;
+        //stack<string> tags;
 
         bool close = false, is_script = false;
 
         char symbol;
         char tag[25], full_attr[10000];
 
-        while(fscanf(fp, "%*[^<]%c %c", &symbol, &tag[0]) == 2) {
+        int ret_f;
+
+        fscanf(fp, " %*[^<]");
+        while((ret_f = fscanf(fp, "%c %c", &symbol, &tag[0])) == 2) {
 
                 if (symbol == '<') {
                         if (tag[0] == '/') {
@@ -160,16 +177,49 @@ vector<pair<string, string> > get_ref_url(char *data, char *url, char *path) {
 
                         printf("\tTAG: %s%s\n", close ? "/" : "", tag);
 
-                        if (is_script == true && !(close == true && strcmp(tag, "script") == 0) )
+
+                        //comments
+
+                        if (strncmp(tag, "!--", 3) == 0) {
+
+                                char test_comment[5000];
+                                test_comment[0] = '\0';
+
+                                //find "-->"
+                                while (1) {
+                                        fscanf(fp, "%*[^-]");
+
+                                        fscanf(fp, "%[-]", test_comment);
+
+                                        if (strlen(test_comment) < 2)
+                                                continue;
+                                        char test_minus;
+                                        fscanf(fp, "%c", &test_minus);
+                                        if (test_minus == '>')
+                                                break;
+
+                                }
+                                fscanf(fp, ">");
+                                fscanf(fp, " %*[^<]");
+                                continue;
+                        }
+
+
+                        if (is_script == true && !(close == true && strcmp(tag, "script") == 0) ) {
+                                printf("\t Not a real tag\n");
+                                fscanf(fp, ">");
+                                fscanf(fp, " %*[^<]");
                                 continue; //Is not a real tag, is part of a Javascript
+                        }
 
                         if (close == true) {
-                                printf("\n\tClose: %s\n", tags.top().c_str());
-                                tags.pop();
+                                //printf("\n\tClose: %s\n", tags.top().c_str());
+                                printf("\n\tClose: %s\n", tag);
+                                //tags.pop();
                         } else {
-                                string temp(tag);
-                                tags.push(temp);
-                                printf("\n\tEnter: %s\n", tags.top().c_str());
+                                //string temp(tag);
+                                //tags.push(temp);
+                                printf("\n\tEnter: %s\n", tag);
                         }
 
                         char temp = '\0';
@@ -179,8 +229,21 @@ vector<pair<string, string> > get_ref_url(char *data, char *url, char *path) {
 
                                 printf("\t Attr: %s\tTemp: %c\n", attr, temp);
 
-                                if (temp == '=')
-                                        fscanf(fp, "\"%[^\"]\"", value);
+                                if (temp == '=') {
+                                        char delim;
+                                        fscanf(fp, "%c", &delim);
+
+                                        printf("\tDelim: %c\n", delim);
+
+                                        if (delim == '\"')
+                                                fscanf(fp, "%[^\"]\"", value);
+                                        else if (delim == '\'')
+                                                fscanf(fp, "%[^\']\'", value);
+                                        else {
+                                                printf("Delimitador estranho: %c\n", delim);
+                                                return urls;
+                                        }
+                                }
                                 else if (temp == '>')
                                         break;
                                 else
@@ -188,10 +251,10 @@ vector<pair<string, string> > get_ref_url(char *data, char *url, char *path) {
 
                                 printf("\t Attr: %s\tValue: %s\n", attr, value);
 
-                                if (value[0] == '\"') {
+                                /*if (value[0] == '\"') {
                                         sscanf(value + 1, "%[^\"]", value); //find string delimited by beginning '\"' and ending '\"'
                                         printf("\tNew value: %s\n", value);
-                                }
+                                }*/
 
                                 for (int i = 0; i < 24; i++) {
 
@@ -220,13 +283,20 @@ vector<pair<string, string> > get_ref_url(char *data, char *url, char *path) {
                         }
                         if (strcmp(tag, "script") == 0) {
                                 is_script = !close;
+                                printf("\tIS SCRIPT: %c\n", is_script ? 'Y' : 'N');
                         }
 
                         fscanf(fp, ">");
+                        fscanf(fp, " %*[^<]");
                 } else {
-                        printf("Unkown error has occurred\n");
+                        printf("Unkown error has occurred: %c %c\n", symbol, tag[0]);
+                        return urls;
                 }
         }
+        printf("\tEND %d\tSymbol: %c\tTag: %c\n", ret_f, symbol, tag[0]);
+
+        fscanf(fp, " %s", data);
+        printf("%s\n", data);
 
         fclose(fp);
 
